474575ed9b3826bce659f960c184b838a9ed1a1d
pedroolopz0 <lopezpedro.arg08@gmail.com>
2026-04-06 02:05:49 -0300

    endpoint que lista canciones

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+import os
 app = FastAPI()
 
 @app.get("/")
@@ -9,3 +10,9 @@ def root():
 @app.get("/ping")
 def ping():
     return {"status": "ok"}
+
+@app.get("/musica")
+def musica():
+    canciones = os.listdir("C:/_pedroolopz")
+    return {"lista": canciones}
+            
